f22f2c529c50467ffdac4cb9cbbe71eba2b67186
Vladimir Zorov <vl.zorov@gmail.com>
2022-07-31 11:42:28 +0300

    Add

--- src/main/java/christmasRaces/entities/cars/SportsCar.java
+++ src/main/java/christmasRaces/entities/cars/SportsCar.java
@@ -2,9 +2,10 @@ package main.java.christmasRaces.entities.cars;
 
 import main.java.christmasRaces.common.ExceptionMessages;
 
-public class MuscleCar implements Car {
+public class SportsCar implements Car{
+    SportsCar sportsCar = new SportsCar();
+    public SportsCar() {
 
-    public MuscleCar() {
     }
 
     @Override
@@ -13,9 +14,9 @@ public class MuscleCar implements Car {
     }
 
     @Override
-    public int getHorsePower() {
+    public int getHorsePower()  {
         int hp = 0;
-        if (getHorsePower() <= 600 && getHorsePower() >= 400) {
+        if (getHorsePower() <= 450 && getHorsePower() >= 250) {
             hp = getHorsePower();
         } else {
             System.out.println(ExceptionMessages.INVALID_HORSE_POWER);
@@ -25,7 +26,7 @@ public class MuscleCar implements Car {
 
     @Override
     public double getCubicCentimeters() {
-        return 5000;
+        return 3000;
     }
 
     @Override
---
+++ src/main/java/christmasRaces/entities/drivers/DriverImpl.java
@@ -0,0 +1,44 @@
+package main.java.christmasRaces.entities.drivers;
+
+import main.java.christmasRaces.entities.cars.Car;
+
+public class DriverImpl implements Driver{
+//    Driver driver = new Driver() {
+//    };
+
+//    public DriverImpl(Driver driver) {
+//        this.driver = driver;
+//    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Car getCar() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfWins() {
+        return 0;
+    }
+
+    @Override
+    public void addCar(Car car) {
+
+    }
+
+    @Override
+    public void winRace() {
+
+    }
+
+    @Override
+    public boolean getCanParticipate() {
+        return false;
+    }
+
+}
+
